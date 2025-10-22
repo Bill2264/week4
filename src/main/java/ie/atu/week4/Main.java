43c239dc7ed2d73bf48b55474dd1d553edd74fb5
Billy Murphy <G00465240@atu.ie>
2025-10-22 17:01:30 +0100

    added comments to code

--- src/main/java/ie/atu/week4/Main.java
+++ src/main/java/ie/atu/week4/Main.java
@@ -9,13 +9,13 @@ public class Main {
         int age = 0;
         Scanner scan1 = new Scanner(System.in);
 
-        while(true) {
+        while(true) {//loops until age is valid
             System.out.println("Please enter your age: ");
             String text = scan1.nextLine().trim();
             try {
-                age = Integer.parseInt(text);
+                age = Integer.parseInt(text);//try to turn string in int
                 break;
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException e) {//if error occurs, prints message
                 System.out.println("Invalid input");
             }
         }
