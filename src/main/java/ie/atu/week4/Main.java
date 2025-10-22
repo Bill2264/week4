b9b0993d8d5acf954a6c5303e5d6034e72378930
Billy Murphy <G00465240@atu.ie>
2025-10-22 16:23:56 +0100

    age input with validity check

--- src/main/java/ie/atu/week4/Main.java
+++ src/main/java/ie/atu/week4/Main.java
@@ -1,9 +1,25 @@
 package ie.atu.week4;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main(String[] args) {
+        int age = 0;
+        Scanner scan1 = new Scanner(System.in);
 
+        while(true) {
+            System.out.println("Please enter your age: ");
+            String text = scan1.nextLine().trim();
+            try {
+                age = Integer.parseInt(text);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input");
+            }
+        }
+        System.out.println("Thanks! the age you entered = " + age);
     }
 }
+
